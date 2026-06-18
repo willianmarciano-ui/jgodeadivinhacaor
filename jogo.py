fe73d45e792495ba657129c201a8676346cec9e3
willianmarciano-ui <willian.marciano@escola.pr.gov.br>
2026-06-18 14:59:04 +0000

    inicio

---
+++ jogo.py
@@ -0,0 +1,18 @@
+print("********************************")
+print("bom vindo ao jogo de adivinhaçao")
+print("********************************")
+
+numerosecreto = 40
+
+chute+input("digite o seu numero")
+print("voce  digitou: ", chute )
+
+chutenumero = int(chute)
+
+#se voce digitar qualquer numero vou verificar se acertou ou errou
+if(numerosecreto == chutenumero)
+print("voce acertou!")
+else:
+ print("voce errou!")
+
+print("fim de jogo")
