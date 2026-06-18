0d2c473ebecfb0d4ceaaa1c6248a2c54c8195392
willianmarciano-ui <willian.marciano@escola.pr.gov.br>
2026-06-18 15:10:40 +0000

    salve

--- jogo.py
+++ jogo.py
@@ -9,10 +9,16 @@ print("voce  digitou: ", chute )
 
 chutenumero = int(chute)
 
+acertou = chutenumero == numerosecreto
+maior = chutenumero > numerosecreto
+menor = chutenumero < numerosecreto
+
 #se voce digitar qualquer numero vou verificar se acertou ou errou
-if(numerosecreto == chutenumero)
-print("voce acertou!")
-else:
- print("voce errou!")
+if(numerosecreto == chutenumero):
+    print("voce acertou!")
+elif(maior):
+    print("voce errou! o seu chute foi maior que o numero secreto.")
+elif(menor):
+    print("voce errou! o seu chute foi menor que o numero secreto.")
 
 print("fim de jogo")
